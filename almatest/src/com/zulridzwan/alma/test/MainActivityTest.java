3ba1c2d03cbdeede120524f59fa47d7683f5a9c8
zulridzwan <zulridzwan@gmail.com>
2014-12-05 18:20:22 +0800

    Updated test cases

--- almatest/src/com/zulridzwan/alma/test/MainActivityTest.java
+++ almatest/src/com/zulridzwan/alma/test/MainActivityTest.java
@@ -1,70 +1,69 @@
 package com.zulridzwan.alma.test;
 
 import com.zulridzwan.alma.MainActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.test.ActivityUnitTestCase;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.zulridzwan.alma.R;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
+public class MainActivityTest extends
+		ActivityUnitTestCase<MainActivity> {
+	
+	private MainActivity activity;
+	private SharedPreferences sharedpreferences;
+	public static final String MyPREFERENCES = "MyPrefs" ;
 
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-
-	//Standard constructor for unit test
 	public MainActivityTest() {
 		super(MainActivity.class);
 	}
-	/*
-	 * The constructor is invoked by the test runner to instantiate the test class, 
-	 * while the setUp() method is invoked by the test runner before it runs any tests 
-	 * in the test class.
-	 */
-	
-	/*
-	 * Typically, in the setUp() method, you should:
 
-Invoke the superclass constructor for setUp(), which is required by JUnit.
-Initialize your test fixture state by:
--Defining the instance variables that store the state of the fixture.
--Creating and storing a reference to an instance of the Activity under test.
--Obtaining a reference to any UI components in the Activity that you want to test.
-You can use the getActivity() method to get a reference to the Activity under test.
-	 */
-	private MainActivity mFirstTestActivity;
-    private TextView tv;
-	
-	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mFirstTestActivity = getActivity();
-        tv = (TextView)mFirstTestActivity.findViewById(R.id.textView1);
+	//https://www.packtpub.com/sites/default/files/3500-chapter-1-getting-started-with-testing.pdf
+	protected void setUp() throws Exception {
+		super.setUp();
+		//Clear shared preference where login info is stored to force login
+		Context c =getInstrumentation().getTargetContext();
+		sharedpreferences=c.getSharedPreferences(MyPREFERENCES,c.MODE_PRIVATE);
+		sharedpreferences.edit().clear().commit();
+		
+		//Get the activity of the tested activity (class)
+		Intent intent = new Intent(c,MainActivity.class);
+		startActivity(intent, null, null);
+		activity = getActivity();
+	}
 
-    }
-	
-/*
- * Add Test Preconditions
-
-As a sanity check, it is good practice to verify that the test fixture has been set up 
-correctly, and the objects that you want to test have been correctly instantiated or 
-initialized. That way, you won’t have to see tests failing because something was wrong 
-with the setup of your test fixture. By convention, the method for verifying your 
-test fixture is called testPreconditions().
- */
-	public void testPreconditions() {
-	    assertNotNull("mFirstTestActivity is null", mFirstTestActivity);
-	    assertNotNull("mFirstTestText is null", tv);
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 	
-/*
- *Add Test Methods to Verify Your Activity
+	public void testLoginLayout(){
+		//Check if text fields and button are loaded
+		assertNotNull(activity.findViewById(R.id.editText1));
+		assertNotNull(activity.findViewById(R.id.editText2));
+		assertNotNull(activity.findViewById(R.id.button1));
+		
+	}
 
-Next, add one or more test methods to verify the layout and functional behavior of your Activity.
+	public void testLoginIntent(){
+		String uname ="admin";
+		String passw ="admin";
+		// Set a value into the text fields
+	    EditText tx_uname = (EditText) activity.findViewById(R.id.editText1);
+	    EditText tx_passw = (EditText) activity.findViewById(R.id.editText2);
+	    tx_uname.setText(uname);
+	    tx_passw.setText(passw);
+	    //Click the login button
+	    Button btn_login = (Button) activity.findViewById(R.id.button1);
+	    btn_login.performClick();
 
-For example, if your Activity includes a TextView, you can add a test method like this 
-to check that it has the correct label text:
- */
-	public void testMyFirstTestTextView_labelText() {
-	    final String expected =
-	            mFirstTestActivity.getString(R.string.hello_world);
-	    final String actual = tv.getText().toString();
-	    assertEquals(expected, actual);
+	    //Verify if SecondActivity was launched by evaluating the triggered intent
+	    Intent triggeredIntent = getStartedActivityIntent();
+	    assertNotNull("Intent should have triggered after button press",triggeredIntent);
+		
+		
 	}
 }
